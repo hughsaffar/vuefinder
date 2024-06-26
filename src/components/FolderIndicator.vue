839ddc32e6393fb021e521120e35477e674b45b2
Hugh Saffar <hughsaffar@gmail.com>
2024-06-26 16:04:07 -0400

    Refactor FolderIndicator.vue

--- src/components/FolderIndicator.vue
+++ src/components/FolderIndicator.vue
@@ -1,11 +1,29 @@
 <template>
-  <div class="h-5 w-5 shrink-0">
-    <div class=" cursor-pointer">
-      <SquareMinusSVG class="text-gray-600" v-if="model" />
-      <SquarePlusSVG class="text-gray-400" v-if="!model" />
+  <div class="vuefinder__folder_indicator">
+    <div class="vuefinder__folder_indicator__wrapper">
+      <SquareMinusSVG class="vuefinder__folder_indicator__minus_icon" v-if="model" />
+      <SquarePlusSVG class="vuefinder__folder_indicator__plus_icon" v-if="!model" />
     </div>
   </div>
 </template>
+
+<style>
+.vuefinder__folder_indicator {
+  @apply h-5 w-5 shrink-0;
+}
+
+.vuefinder__folder_indicator__wrapper {
+  @apply cursor-pointer;
+}
+
+.vuefinder__folder_indicator__minus_icon {
+  @apply text-gray-600;
+}
+
+.vuefinder__folder_indicator__plus_icon {
+  @apply text-gray-400;
+}
+</style>
 
 
 <script setup>
