fa3474d6df98ac746aaad9984003a67fc2639e94
Hugh Saffar <hughsaffar@gmail.com>
2024-06-26 14:51:07 -0400

    Refactor TreeSubfolderList.vue

--- src/components/TreeSubfolderList.vue
+++ src/components/TreeSubfolderList.vue
@@ -1,26 +1,65 @@
 <template>
-  <ul ref="parentSubfolderList" class="block">
+  <ul ref="parentSubfolderList" class="vuefinder__tree">
     <li v-for="(item, index) in treeSubFolders"
-        class="flex flex-col space-x-0.5 py-0.5 text-sm" :key="item.path">
-      <div class="flex hover:text-sky-700 dark:hover:text-sky-200/50 rounded ">
-        <div class="h-5 w-5 shrink-0"  @click="showSubFolders[item.path] = !showSubFolders[item.path]">
+        class="vuefinder__tree__item" :key="item.path">
+      <div class="vuefinder__tree__item__header">
+        <div class="vuefinder__tree__item__header__icon"  @click="showSubFolders[item.path] = !showSubFolders[item.path]">
           <FolderLoaderIndicator :adapter="adapter" :path="item.path" v-model="showSubFolders[item.path]" />
         </div>
-        <div class="flex cursor-pointer" :title="item.path"
+        <div class="vuefinder__tree__item__header__title" :title="item.path"
             @click="app.emitter.emit('vf-fetch', {params:{q: 'index', adapter: props.adapter, path:item.path}})">
-          <div class="h-5 w-5 shrink-0">
+          <div class="vuefinder__tree__item__header__title__icon">
             <OpenFolderSVG v-if="app.fs.path === item.path"/>
             <FolderSVG v-else/>
           </div>
-          <div class="text-nowrap pr-4" :class="{'underline decoration-blue-300 dark:decoration-gray-400' : app.fs.path === item.path}">{{ item.basename }}</div>
+          <div class="vuefinder__tree__item__header__title__text" :class="{'vuefinder__tree__item__header__title__text--active' : app.fs.path === item.path}">{{ item.basename }}</div>
         </div>
       </div>
-      <div class="pl-4">
+      <div class="vuefinder__tree__item__content">
         <TreeSubfolderList :adapter="props.adapter" :path="item.path" v-show="showSubFolders[item.path]" />
       </div>
     </li>
   </ul>
 </template>
+
+<style>
+.vuefinder__tree {
+  @apply block;
+}
+
+.vuefinder__tree__item {
+  @apply flex flex-col space-x-0.5 py-0.5 text-sm;
+}
+
+.vuefinder__tree__item__header {
+  @apply flex hover:text-sky-700 dark:hover:text-sky-200/50 rounded;
+}
+
+.vuefinder__tree__item__header__icon {
+  @apply h-5 w-5 shrink-0;
+}
+
+.vuefinder__tree__item__header__title {
+  @apply flex cursor-pointer;
+}
+
+.vuefinder__tree__item__header__title__icon {
+  @apply h-5 w-5 shrink-0;
+}
+
+.vuefinder__tree__item__header__title__text {
+  @apply text-nowrap pr-4;
+}
+
+.vuefinder__tree__item__header__title__text--active {
+  @apply underline decoration-blue-300 dark:decoration-gray-400;
+}
+
+.vuefinder__tree__item__content {
+  @apply pl-4;
+}
+
+</style>
 <script setup>
 import {computed, inject, onMounted, ref} from 'vue';
 
