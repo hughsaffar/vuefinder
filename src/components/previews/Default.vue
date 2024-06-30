c27726ecd92a92aaf6dc6fcfe4a0e728fcccda61
Hugh Saffar <hughsaffar@gmail.com>
2024-06-30 15:56:26 -0400

    Refactor Default.vue

--- src/components/previews/Default.vue
+++ src/components/previews/Default.vue
@@ -1,10 +1,18 @@
 <template>
   <div class="flex">
-    <h3 class="mb-2 text-lg leading-6 font-medium text-gray-900 dark:text-gray-400" id="modal-title"
+    <h3 class="vuefinder__default_preview_modal__title" id="modal-title"
          :title="app.modal.data.item.path">{{ app.modal.data.item.basename }}</h3>
   </div>
   <div></div>
 </template>
+
+<style>
+
+.vuefinder__default_preview_modal__title {
+  @apply mb-2 text-lg leading-6 font-medium text-gray-900 dark:text-gray-400l;
+}
+
+</style>
 
 <script setup>
 
